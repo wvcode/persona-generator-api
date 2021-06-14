fa030e7720ff1d14f9eb39a505cba4085bba175f
Walter Ritzel <walter_ritzel@dell.com>
2021-06-14 18:46:40 -0300

    fix: new api

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -k uvicorn.workers.UvicornWorker app:app --max-requests 12 --timeout 70
---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+'''
+    Simple API
+'''
+import base64
+import json
+from time import time
+import requests
+
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.middleware.gzip import GZipMiddleware
+
+app = FastAPI(title='API Model')
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+@app.middleware('http')
+async def remove_file_before_leave(request: Request, call_next):
+  start_time = time()
+  response = await call_next(request)
+  process_time = time()
+  response.headers['X-Process-Time'] = str(process_time - start_time)
+  return response
+
+
+@app.get('/', tags=['Version 1'])
+async def get_main():
+  response = requests.get('https://randomuser.me/api/')
+  return response.json()
+
+if __name__ == '__main__':
+ uvicorn.run(app, host='0.0.0.0', port=8000, debug=True)
